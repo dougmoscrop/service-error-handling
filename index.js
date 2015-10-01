87105270da7b3909d1c59f4ac6c69d5e70cf6315
Doug Moscrop <doug.moscrop@gmail.com>
2015-10-01 08:25:42 -0400

    Start

--- index.js
+++ index.js
@@ -1,62 +1,34 @@
-var httpErrors = require('http-errors'),
-	errorHandler = require('errorhandler');
+var errorHandler = require('errorhandler');
 
-module.exports = function(opts) {
-	var handlers = [];
+errorHandler.title = process.env.npm_package_name || 'Error';
 
-	opts = opts || {};
+module.exports = function() {
+  if (process.env.NODE_ENV === 'production') {
+    return function(err, req, res, next) {
+      var status = err.status || err.statusCode,
+      expose = err.expose || status < 500;
 
-	if (typeof opts.mongo === 'undefined') {
-		opts.mongo = true;
-	}
+      var error = {};
 
-	if (opts.mongo) {
-		handlers.push(function(err, req, res, next) {
-			 // Duplicate key error
-			if (err.code === 11000 || err.code === 11001) {
-				next(new httpErrors.Conflict(err.message));
-				return;
-			}
+      if (expose && status) {
+        if (err.message) {
+          error.message = err.message;
+        }
 
-			if (err.name === 'CastError' || err.name === 'ValidationError') {
-				next(new httpErrors.BadRequest(err.message));
-				return;
-			}
+        if (err.errors) {
+          error.errors = err.errors;
+        }
+      } else {
+        status = 500;
+        error.message = 'Internal Server Error';
+      }
 
-			next(err);
-		});
-	}
-
-	if (opts.development) {
-		if (typeof opts.title !== 'undefined') {
-			errorHandler.title = opts.title;
-		}
-
-		handlers.push(errorHandler({log: opts.log}));
-	} else {
-		handlers.push(function(err, req, res, next) {
-			var status = err.status || err.statusCode,
-				expose = err.expose || status < 500;
-			
-			var error = {};
-			
-			if (expose && status) {
-				if (err.message) {
-					error.message = err.message;
-				}
-				
-				if (err.errors) {
-					error.errors = err.errors
-				}
-			} else {
-				status = 500;
-				error.message = 'Internal Server Error';
-			}
-			
-			res.status(status);
-			res.json(error);
-		});
-	}
-
-	return handlers;
+      res.status(status);
+      res.json(error);
+    };
+  } else {
+    return errorHandler({
+      log: false
+    });
+  }
 };
--- test/server.js
+++ test/server.js
@@ -4,25 +4,25 @@ var express = require('express'),
 var server = express();
 
 server.get('/error', function(req, res, next) {
-	var error = new Error(req.query.message || 'Error!');
-	
-	if (req.query.code) {
-		error.code = Number(req.query.code);
-	}
-	
-	if (req.query.name) {
-		error.name = req.query.name;
-	}
-	
-	next(error);
+  var error = new Error(req.query.message || 'Error!');
+
+  if (req.query.code) {
+    error.code = Number(req.query.code);
+  }
+
+  if (req.query.name) {
+    error.name = req.query.name;
+  }
+
+  next(error);
 });
-	
+
 server.use(errorHandling({
-	development: process.env.NODE_ENV !== 'production'
+  development: process.env.NODE_ENV !== 'production'
 }));
 
 if (require.main === module) {
-	server.listen(8080);
+  server.listen(8080);
 }
 
 module.exports = server;
